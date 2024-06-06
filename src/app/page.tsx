941e485b4bca97babcc519fd0cbff811906feaca
Sh9hid <abdullah.cool1@gmail.com>
2024-06-06 23:27:44 +0530

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Navbar from "@/components/navbar";
+import footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <main>
-     <Navbar />
-    </main>
+    <><main>
+      <Navbar />
+    </main><footer /></>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,5 @@
+const footer =  () => {
+    return (<p>Made by Sh9</p>)
+}
+
+export default footer
